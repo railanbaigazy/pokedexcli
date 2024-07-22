b49f8a99fab5af16e149b2f7a201cb18404af0fd
railanbaigazy <baigazy.railan@gmail.com>
2024-07-22 18:26:59 +0500

    pokeapi

---
+++ internal/pokeapi/location_area_req.go
@@ -0,0 +1,41 @@
+package pokeapi
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func (c *Client) ListLocationAreas() (LocationAreasResp, error) {
+	endpoint := "/location-area"
+	fullURL := baseURL + endpoint
+
+	req, err := http.NewRequest("GET", fullURL, nil)
+	if err != nil {
+		return LocationAreasResp{}, err
+	}
+	defer req.Body.Close()
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return LocationAreasResp{}, err
+	}
+
+	if res.StatusCode > 399 {
+		return LocationAreasResp{}, fmt.Errorf("Bad status code: %v", res.StatusCode)
+	}
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return LocationAreasResp{}, err
+	}
+
+	locationAreasResp := LocationAreasResp{}
+	err = json.Unmarshal(data, &locationAreasResp)
+	if err != nil {
+		return LocationAreasResp{}, err
+	}
+
+	return locationAreasResp, nil
+}
